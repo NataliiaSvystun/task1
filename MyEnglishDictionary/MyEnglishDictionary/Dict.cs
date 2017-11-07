7504b1c42d8d11c2428251207aec371545045348
Nataliia Svystun <nat20088@ukr.net>
2017-11-08 00:02:23 +0200

    myCustomDictionary

---
+++ MyEnglishDictionary/MyEnglishDictionary/Dict.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace MyEnglishDictionary
+{
+    public class Dict<K, V> : IEnumerable<KeyValuePair<K, V>>
+    {
+        private const int INITIAL_SIZE = 16;
+        LinkedList<KeyValuePair<K, V>>[] values;
+
+        public Dict()
+        {
+            this.values = new LinkedList<KeyValuePair<K, V>>[INITIAL_SIZE];
+        }
+
+        public int Count { get; private set; }
+        public int Capacity
+        {
+            get
+            {
+                return this.values.Length;
+            }
+        }
+
+        public void Add(K key, V value)
+        {
+            var hash = this.HashKey(key);
+
+            if (this.values[hash] == null)
+            {
+                this.values[hash] = new LinkedList<KeyValuePair<K, V>>();
+            }
+
+            var keyExistsAlready = this.values[hash].Any(p => p.Key.Equals(key));
+
+            if (keyExistsAlready)
+            {
+                throw new ArgumentException("Key already exitst. You cannot add 2 elements with the same key!");
+            }
+
+            var pair = new KeyValuePair<K, V>(key, value);
+            this.values[hash].AddLast(pair);
+            this.Count++;
+
+            if (this.Count >= 0.75 * this.Capacity)
+            {
+                this.ResizeAndReAddValues();
+            }
+        }
+
+        public V Find(K key)
+        {
+            var hash = HashKey(key);
+
+            if (this.values[hash] == null)
+            {
+                return default(V);
+            }
+            var collection = this.values[hash];
+            //try
+            //{
+                var result = collection.First(p => p.Key.Equals(key)).Value;
+
+                return result;
+            //}
+            //catch (InvalidOperationException ex)
+            //{
+            //    Console.WriteLine("Not found");
+            //    return null;
+            //}
+
+
+        }
+
+        public bool ContainsKey(K key)
+        {
+            var hash = HashKey(key);
+
+            if (this.values[hash] == null)
+            {
+                return false;
+            }
+            var collection = this.values[hash];
+            return collection.Any(pair => pair.Key.Equals(key));
+        }
+
+        private int HashKey(K key)
+        {
+            var hash = Math.Abs(key.GetHashCode()) % this.Capacity;
+            return hash;
+        }
+        private void ResizeAndReAddValues()
+        {
+            //cache values
+            var cachedValues = this.values;
+
+            //resize
+            this.values = new LinkedList<KeyValuePair<K, V>>[2 * this.Capacity];
+
+            //Add values
+            this.Count = 0;
+            foreach (var collection in cachedValues)
+            {
+                if (collection != null)
+                {
+                    foreach (var value in collection)
+                    {
+                        this.Add(value.Key, value.Value);
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var collection in this.values)
+            {
+                if (collection != null)
+                {
+                    foreach (var value in collection)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+
+    }
+}
---
+++ MyEnglishDictionary/MyEnglishDictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyEnglishDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Dict<string, string> myDictionary = new Dict<string, string>();
+            myDictionary.Add("мама", "mother");
+            myDictionary.Add("тато", "father");
+            myDictionary.Add("сестра", "sister");
+            myDictionary.Add("брат", "brother");
+            myDictionary.Add("тітка", "aunt");
+            myDictionary.Add("дядько", "uncle");
+
+            //You can not Add element with the same key
+            //myDictionary.Add("мама", "mom");
+
+            Console.WriteLine("All words in my dictionary");
+
+            foreach (KeyValuePair<string,string> item in myDictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            //or
+
+            foreach (var item in myDictionary)
+            {
+                Console.WriteLine("{0}, {1}",item.Key,item.Value);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Can you translate - мама?");
+            if (myDictionary.ContainsKey("мама"))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
